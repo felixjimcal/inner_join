3ca1b49bce5d921ed502b355bff98807aa4dd107
Félix Jiménez Calvo <felixjcalvo@gmail.com>
2022-06-28 16:07:47 +0200

    Update main.cpp
    Refactoring and optimizing code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
-#include <assert.h>
 #include <iostream>
-#include <vector>
+#include <assert.h>
 
 struct Users {
   int id;
@@ -22,48 +21,23 @@ struct JoinedData {
 void CheckJoin(JoinedData result) {
   JoinedData expectedOuptut{1, "Carla", "carlasemail@gmail.com"};
 
+  assert(result.id == expectedOuptut.id);
   assert(result.Name == expectedOuptut.Name);
   assert(result.Email == expectedOuptut.Email);
 }
 
-
 int main() {
-  Users users[6] = {
-      {0, "Felix"},  {1, "Carla"},  {2, "Atlas"},
-      {3, "Anibal"}, {4, "Aurora"}, {5, "Geneve"},
-  };
+  Users users[6] = { {0, "Felix"},  {1, "Carla"},  {2, "Atlas"}, {3, "Anibal"}, {4, "Aurora"}, {5, "Geneve"} };
 
-  Emails emails[2] = {{1, "carlasemail@gmail.com"},
-                      {99, "someonelse@gmail.com"}};
+  Emails emails[2] = { {1, "carlasemail@gmail.com"}, {99, "someonelse@gmail.com"}};
 
-  int id = 1;
+  auto matchUser = std::find_if(std::begin(users), std::end(users), [](const Users &mu) { return mu.id == 1; });
+  auto matcEmail = std::find_if(std::begin(emails), std::end(emails), [](const Emails &me) { return me.id == 1; });
+
   JoinedData result;
-
-  // https://stackoverflow.com/questions/589985/vectors-structs-and-stdfind
-  struct findUserData
-  {
-      int id;
-      findUserData(int id) : id(id) {}
-      bool operator () ( const Users& m ) const
-      {
-          return m.id == id;
-      }
-  };
-  auto userData = std::find_if(std::begin(users), std::end(users), findUserData(id));
-
-  struct findUserEmail
-  {
-      int id;
-      findUserEmail(int id) : id(id) {}
-      bool operator () ( const Emails& m ) const
-      {
-          return m.id == id;
-      }
-  };
-  auto userEmail = std::find_if(std::begin(emails), std::end(emails), findUserEmail(id));
-
-  result = {userData->id, userData->Name, userEmail->address};
+  result = {matchUser->id, matchUser->Name, matcEmail->address};
 
   CheckJoin(result);
+
   return 0;
 }
